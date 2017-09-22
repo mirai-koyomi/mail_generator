3173255a95ccd9ba5483b1d3f5f79cee7337c2d5
mirai_koyomi <crossclose00@gmail.com>
2017-09-22 18:52:39 +0900

    現状までの進捗分

--- lib/mail_generator.rb
+++ lib/mail_generator.rb
@@ -1,5 +1,12 @@
 require "mail_generator/version"
+require "mail_generator/backlog"
+require "mail_generator/Util"
 
 module MailGenerator
-  # Your code goes here...
+	def apc_generate(ticket, branch, reserved, apply_date, release_date)
+	end
 end
+	
+	secret = MailGenerator::Util.load_secret_file
+	client = MailGenerator::Backlog::API.new(secret['space_id'], secret['api_key'], secret['project_key'])
+	git = MailGenerator::Git.new()
